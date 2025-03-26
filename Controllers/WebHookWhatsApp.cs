7b491e733e0636bbac8abd309cb01304f695c63e
Matheus Melo Correa <matheusmelocorrea@gmail.com>
2025-03-26 09:56:56 -0300

    TesteMetodoPost

--- Controllers/WebHookWhatsApp.cs
+++ Controllers/WebHookWhatsApp.cs
@@ -1,14 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
 
 [ApiController]
 [Route("api/webhook")]
 public class WhatsAppWebhookController : ControllerBase
 {
-    [HttpGet]
-    public string dadosMocado()
-    {
-        Console.WriteLine("Nada");
+    private readonly HttpClient _httpClient;
+    private const string Token = "EAAHV0JpFtmIBO5tfWsVfMVYvNZBdKUTAlhsZCHmTOXI0l6Q62zBRIRFytTvfnZA29BKTbDr2b05yCpcELmUYiYkbSwC6OoaJuT0W98naGZA8xy18HvDO5J8tOYfVRUEPaKlPOAvFnpgD1me1JD2XohD2RleAyHLAbYY7EV9xerVaxOeT2cJQeSNcNRNdUzIJue2387oVssmJvOGWN0ZBrLxmgWHcZD";
 
-        return "a";
+
+    public WhatsAppWebhookController()
+    {
+        _httpClient = new HttpClient();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] JObject bodyParam)
+    {
+        if (bodyParam["object"] != null)
+        {
+            var entry = bodyParam["entry"]?.FirstOrDefault();
+            var changes = entry?["changes"]?.FirstOrDefault();
+            var value = changes?["value"];
+            var messages = value?["messages"]?.FirstOrDefault();
+
+            if (messages != null)
+            {
+                string phoneNumberId = value?["metadata"]?["phone_number_id"]?.ToString();
+                string from = messages?["from"]?.ToString();
+                string msgBody = messages?["text"]?["body"]?.ToString();
+
+                Console.WriteLine($"Phone number: {phoneNumberId}");
+                Console.WriteLine($"From: {from}");
+                Console.WriteLine($"Message: {msgBody}");
+
+                var responseBody = new
+                {
+                    messaging_product = "whatsapp",
+                    to = from,
+                    text = new { body = $"Hi.. I'm Prasath, your message is {msgBody}" }
+                };
+
+                var jsonContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(responseBody), Encoding.UTF8, "application/json");
+
+
+                await _httpClient.PostAsync($"https://graph.facebook.com/v13.0/{phoneNumberId}/messages?access_token={Token}", jsonContent);
+
+                return Ok();
+            }
+        }
+        return NotFound();
     }
 }
