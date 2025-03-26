2722d4167863c2d5d7946bcdab8e136cd82c2475
Matheus Melo Correa <matheusmelocorrea@gmail.com>
2025-03-26 19:11:58 -0300

    AdcGet

--- Controllers/WebHookWhatsApp.cs
+++ Controllers/WebHookWhatsApp.cs
@@ -13,8 +13,20 @@ public class WebhookController : ControllerBase
         _criarUsuarioProducer = criarNovoUsuarioProducer;
     }
 
+    [HttpGet]
+    public IActionResult VerifyWebhook([FromQuery] string hub_mode, [FromQuery] string hub_challenge, [FromQuery] string hub_verify_token)
+    {
+        var verifyToken = "EAAHV0JpFtmIBO7dSkZA8vZAia41U98VMA5G4vw5LVUUsmleVpdcV4o1b6ZC60dFnl2nFAZBvKGpk4yXYMXYZC24Knes4SQxoDcX1NkelYLTP8hSCP1qA39ahoCOVIXm5QE7yQIwXZBZCWtZAOOj1IAMpOGK82nLuShVYFSssUEYQMYLzj8YvWT0bfRyFunxgzhOypZBMUThZADJAdk9YRgeEcBIVsgem8ZD"; // O mesmo token que você configurou no portal do Meta
 
+        // Verifica se os parâmetros estão corretos e se o token bate
+        if (hub_mode == "subscribe" && hub_verify_token == verifyToken)
+        {
+            return Ok(hub_challenge); // Retorna o desafio para confirmar a inscrição
+        }
 
+        return BadRequest("Token de verificação inválido");
+    }
+    [Route("api/webhook/criarusuario")]
     [HttpPost]
     public async Task<string> ReceiveMessage([FromBody] JsonElement body)
     {
